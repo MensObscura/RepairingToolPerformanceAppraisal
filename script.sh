66fdc7a7cc564723bda951359cf1bc6bc81ac609
Rosa Thibault <rosa@b10p10.fil.univ-lille1.fr>
2016-01-28 21:07:44 +0100

    life is awful somtimes

--- script.sh
+++ script.sh
@@ -1,10 +1,11 @@
 #bin/bash
 
 
-
 #DL sources nopol intro-class finch epr-asm
+
 mkdir soft
 cd soft
+path=`pwd`
 
 git clone https://github.com/SpoonLabs/nopol.git
 
@@ -12,30 +13,75 @@ git clone https://github.com/eschulte/asm.git
 
 wget http://finch.cs.bgu.ac.il/dl/finch.zip
 unzip finch.zip -d .
-rm finch.zip
 
 git clone https://github.com/Spirals-Team/IntroClassJava.git
 
-#git clone git://orgmode.org/org-mode.git
-#apt-get install  tangled
-#git clone https://github.com/technomancy/leiningen.git
+
+#Clean Trash sources
+
+rm finch.zip
 
 
-#Compiling Nopol
+#Nopol Dependencies & requirements
 
-export JAVA_HOME=/usr/lib/java-7-openjdk-amd64
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u79-b15/jdk-7u79-linux-x64.tar.gz
+tar xzvf jdk-7u79-linux-x64.tar.gz
+
+java=$JAVA_HOME
+export JAVA_HOME=$path/jdk-7u79-linux-x64/jre
+
+
+#Compiling nopol
+
+cd nopol
 
 mvn install -Dmaven.test.skip=true 
 
-export JAVA_HOME=/usr/lib/java-8-oracle
+
+#Clean nopol trash
+
+cd ..
+rm jdk-7u79-linux-x64.tar.gz
+rm -rf jdk-7u79-linux-x64
+export JAVA_HOME=$java
+
+
+#epr-asm dependencies requirements
+
+git clone git://orgmode.org/org-mode.git
+
+git clone https://github.com/technomancy/leiningen.git
+cd leiningen
+git checkout stable
+PATH=$PATH:$path/leiningen/bin/
+chmod 755 $path/leiningen/bin/lein
+cd leiningen-core/
+lein bootstrap
+
+apt-get install  tangled
+
+
+#Compiling asm
+
+cd ../asm/
+
+lein deps
+
+tangle neutral.org
+
+lein jar
+
+
+#add it to classpath
+
 
 
 
 #Running all IntroClass
 
-#mvn test
+mvn test
 
-#java -jar nopol-0.0.3-SNAPSHOT-jar-with-dependencies.jar -s src/main/java/:src/test/#java/ -c target/test-classes/:target/classes/
+java -jar nopol-0.0.3-SNAPSHOT-jar-with-dependencies.jar -s src/main/java/:src/test/#java/ -c target/test-classes/:target/classes/
 
 #Change variable
 #rootJavaIntroclass= os.path.dirname(__file__) + "./../dataset"
