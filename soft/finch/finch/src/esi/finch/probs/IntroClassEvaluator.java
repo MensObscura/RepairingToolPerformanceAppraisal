b9cc0bad7f75f32aca58f39ef910a0214dc8bade
rosa <thibaultrosa@gmail.com>
2016-02-07 03:34:57 +0100

    working but 0 resulté

--- soft/finch/finch/src/esi/finch/probs/IntroClassEvaluator.java
+++ soft/finch/finch/src/esi/finch/probs/IntroClassEvaluator.java
@@ -1,7 +1,8 @@
 package esi.finch.probs;
 
-import java.lang.reflect.Constructor;
+
 import java.lang.reflect.Method;
+
 
 import org.apache.commons.logging.Log;
 import org.junit.runner.JUnitCore;
@@ -9,75 +10,34 @@ import org.junit.runner.JUnitCore;
 import ec.util.MersenneTwisterFast;
 import esi.finch.ecj.bc.BytecodeEvaluator;
 import esi.finch.ecj.bc.BytecodeIndividual;
-import esi.finch.probs.ArtificialAnt.OperationsLimit;
 import esi.util.Config;
-import esi.util.SandBox;
-import java.lang.ClassLoader;
 
 public class IntroClassEvaluator implements BytecodeEvaluator {
 
 	private static final Log log = Config.getLogger();
-	private static final String EXEC		 = "exec";
+	public  static Class klass;
 	@Override
 	public Result evaluate(BytecodeIndividual ind, long timeout, long steps, MersenneTwisterFast random,
 			int threadnum) {
 
 
-		Method exec;
 
-		Class klass = ind.getMethod().getDeclaringClass();
+		this.klass = ind.getMethod().getDeclaringClass();
 
-		try{
-			exec = klass.getDeclaredMethod(EXEC);
-		} catch (Exception e) {
-			throw new Error("Unexpected exception", e);
-		}
-		// Create an artificial ant instance
-		Object instance;
-		try {
-			Constructor<?> konst = klass.getConstructor();
-			instance = konst.newInstance();
-
-			// Save instance for representation purposes
-			ind.setInfo(instance);
-		} catch (Exception e) {
-			throw new Error("Unexpected exception", e);
-		}
-
-		SandBox sandbox = new SandBox(instance, exec, timeout);
-
-		// Execute the "go" method (exception is thrown when maximum steps reached)
-		boolean ideal = false;
-		boolean valid = true;
-		float finess = 0;
-
-		SandBox.Result result = sandbox.call();
-
-		// Timeout invalidates the individual
-		if (result == null) {
-			log.debug("Timeout in " + ind);
-			valid = false;
-		}
-		// Unexpected exception invalidates the individual
-		else if (result.exception != null  &&  !(result.exception instanceof OperationsLimit)) {
-			log.debug("Exception: " + result.exception + " in " + ind);
-			valid = false;
-		}
-		else {
+		
+			boolean ideal = false;
+			boolean valid = true;
+			float finess = 0;
 			org.junit.runner.Result resultTest = null;
-			try {
-				Class<?> test = Class.forName("syllables_fcf701e8_002WhiteboxTest");
-				resultTest =JUnitCore.runClasses(test);
-			} catch (ClassNotFoundException e) {
-				System.out.println("Class not found : " +e );
-			}
+			Class test = new syllables_fcf701e8_002WhiteboxTest().getClass();
+			JUnitCore ju = new JUnitCore();
+			resultTest =ju.runClasses(test);
 
 			if(resultTest != null){
 				ideal = resultTest.wasSuccessful();
 				finess = resultTest.getRunCount() - resultTest.getFailureCount();
 				System.out.println("IntroClassEvaluator : finess = "+ resultTest.getRunCount() +"-"+ resultTest.getFailureCount());
 			}
-		}
 
 		//if all test green, then result true else false
 		return new Result(finess, ideal);
@@ -90,4 +50,7 @@ public class IntroClassEvaluator implements BytecodeEvaluator {
 		return null;
 	}
 
+	public  static Class getGeneratedClass(){
+		return klass;
+	}
 }
